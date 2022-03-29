7c0a77c96aea28c5c0cddcb2f1b142e3d85515c1
Faun <oleksnelder@gmail.com>
2022-03-29 19:49:51 +0200

    database but it works now

--- database.js
+++ database.js
@@ -3,27 +3,30 @@ const file = "database.db";
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(file);
 
-db.run(`--sql
-    create table users (
-        id int primary key not null,
-        email text not null,
-        password text not null,
-        address text
-    )
-
-    create table dishes (
-        id int not null,
-        name text not null,
-        price int not null,
-        img text
-    )
-
-    create table orders (
-        id int primary key not null,
-        user_id int not null,
-        total int not null,
-        orderdata text not null
-    )
-`);
+db.serialize(() => {
+    db
+    .run(`--sql
+        CREATE TABLE if not exists users (
+            id int primary key not null,
+            email text not null,
+            password text not null,
+            address text
+    );`)
+    .run(`--sql
+        CREATE TABLE if not exists dishes (
+            id int primary key not null,
+            name text not null,
+            price int not null,
+            img text
+        );`)
+    .run(`--sql
+        CREATE TABLE if not exists orders (
+            id int primary key not null,
+            userid int not null,
+            total int not null,
+            orderdata text not null
+        );
+    `);
+});
 
 module.exports = db;
