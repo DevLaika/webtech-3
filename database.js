0e8e4f53724f817e79d7f26ff25f3ec601db0e1e
Faun <oleksnelder@gmail.com>
2022-03-29 19:00:47 +0200

    database table setup but it doesnt work yet

--- database.js
+++ database.js
@@ -3,4 +3,27 @@ const file = "database.db";
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(file);
 
+db.run(`--sql
+    create table users (
+        id int primary key not null,
+        email text not null,
+        password text not null,
+        address text
+    )
+
+    create table dishes (
+        id int not null,
+        name text not null,
+        price int not null,
+        img text
+    )
+
+    create table orders (
+        id int primary key not null,
+        user_id int not null,
+        total int not null,
+        orderdata text not null
+    )
+`);
+
 module.exports = db;
--- routes/api_router.js
+++ routes/api_router.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database');
 
-
+router.post('/user', (err, req, res) => {
+    
+})
 
 router.get('/user', (req, res) => {
 
