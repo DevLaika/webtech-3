21617e3d6c1af9767b4f63a9d142f99b04efeb02
Faun <oleksnelder@gmail.com>
2022-03-30 16:57:01 +0200

    werkcollege dingen x

--- database.js
+++ database.js
@@ -9,26 +9,60 @@ db.serialize(() => {
     db
     .run(`--sql
         CREATE TABLE if not exists users (
-            id int primary key not null,
+            userid int primary key not null,
             email text not null,
+            name text not null,
             password text not null,
-            address text
+            address_id int FOREIGN KEY REFERENCES addresses(id)
+    );`)
+    .run(`--sql
+        CREATE TABLE if not exists addresses (
+            id int primary key not null,
+            country text not null,
+            city text not null,
+            postalcode text not null,
+            street text not null,
+            number text not null
     );`)
     .run(`--sql
         CREATE TABLE if not exists dishes (
             id int primary key not null,
             name text not null,
             price int not null,
-            img text
+            img text,
+            category text
         );`)
     .run(`--sql
         CREATE TABLE if not exists orders (
             id int primary key not null,
-            userid int not null,
+            userid int not null FOREIGN key REFERENCES users(id),
             total int not null,
-            orderdata text not null
+            datetime int not null,
+            addressId int not null foreign key refrences addresses(id)
         );
-    `);
+    `)
+    .run(`--sql
+        create table if not exists carts (
+            id int primary key not null,
+            userid int not null FOREIGN key REFERENCES users(id)
+        );`
+    )
+    .run(`--sql
+        create table if not exists orderdishes (
+            id int primary key not null,
+            order_id int not null FOREIGN key REFERENCES orders(id),
+            dish_id int not null FOREIGN key REFERENCES dishes(id),
+            quantity int not null
+        );
+    `)
+    .run(`--sql
+        create table if not exists cartdishes (
+            id int primary key not null,
+            cart_id int not null FOREIGN key REFERENCES carts(id),
+            dish_id int not null FOREIGN key REFERENCES dishes(id),
+            quantity int not null
+        );
+    `)
 });
 
 // Export the database object for use elsewhere
--- routes/api_router.js
+++ routes/api_router.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database');
 
+// Login
+router.get('/auth', (err, req, res) => {
+
+})
+
+// Get dishes
+router.get('/dishes', (err, req, res) => {
+    // Load 10 without auth, all with
+})
+
 // Register a user
 router.post('/user', (err, req, res) => {
 
@@ -15,6 +25,37 @@ router.patch('/user', (err, req, res) => {
 // Get user info
 router.get('/user', (req, res) => {
 
+})
+
+// Delete user
+router.delete('/user', (err, req, res) => {
+
+})
+
+// Get order history
+router.get('/oderhistory', (err, req, res) => {
+
+})
+
+// Delete order history
+router.delete('/oderhistory', (err, req, res) => {
+
+})
+
+router.get('/cart', (err, req, res) => {
+    // Get userid's cart if exists
+})
+
+router.post('/cart', (err, req, res) => {
+    // Add item to cart, create new cart row if nescesary
+})
+
+router.patch('/cart', (err, req, res) => {
+    // Change amount of item in cart of userid.
+})
+
+router.delete('/cart', (err, req, res) => {
+    // Delete item from cart of userid if specified, otherwise clear cart of userid.
 })
 
 // Export api routes
