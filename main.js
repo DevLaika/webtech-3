84dd373ecaa62844d6e2aee250a4c3de5028b58c
Charles Klijnman <99817354+charleskl100@users.noreply.github.com>
2022-04-14 20:59:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,46 @@ app.use(express.json());
 // Use the api functionality
 app.use('/api', api_router);
 
+// register functionality
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.get('./public/register.html', (req, res) =>
+  {res.render('./public/register.html');
+  })
+app.post('/register', async (req, res) => {
+  try{
+    const {email, name, password} = await req.body;
+    const hash = await bcrypt.hash(password, 10);
+    res.status(200); 
+    const insert = db.prepare('INSERT INTO users(email, name, password) VALUES(?, ?, ?)');
+    insert.run(email, name, hash);
+    res.redirect("/login.html");
+  }catch(e) {
+    console.log(e);
+    res.status(500).send('something broke');
+  }
+});
+
+// login functionality
+app.post('/login', async (req, res) => {
+  try { 
+    const {email, password} = req.body;
+    const row = db.prepare('SELECT * FROM users WHERE email = ?').get(email);
+    const validpass =  await bcrypt.compare(password, row.password);
+    if(validpass){
+      res.status(200).json('valid login credentials');
+    }
+  } catch(e){
+    console.log(e)
+    res.status(500).send('Wrong name or password!');
+  }
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 // Start the app, listens for requests on port 8022
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
